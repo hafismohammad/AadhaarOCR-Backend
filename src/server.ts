f8f0fd7f59ad66d95c5b266136e1fea414ad4702
Hafis Mohammad <hafismhdthaleekara764@gmail.com>
2025-03-12 20:09:58 +0530

    origin changed

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,11 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 const corsOptions = {
-  origin: ['http://localhost:5173', 'http://localhost:5174'], 
+  origin: [
+    'https://aadhaar-ocr-theta.vercel.app',
+    'http://localhost:5173',
+    'http://localhost:5174'
+        ], 
  
   credentials: true, 
 };
