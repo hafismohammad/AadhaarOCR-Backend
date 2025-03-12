1844922d4abe71fc54df9a4e7488aeb0abc98673
Hafis Mohammad <hafismhdthaleekara764@gmail.com>
2025-03-12 16:59:02 +0530

    Fix: origin issue

--- src/server.ts
+++ src/server.ts
@@ -9,16 +9,18 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 const corsOptions = {
-  origin: `${process.env.CORS_ORIGIN}`, 
+  origin: ['http://localhost:5173', 'http://localhost:5174'], 
  
   credentials: true, 
 };
+const PORT = process.env.PORT || 4000;
 
 app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use('/',route)
 
-app.listen(process.env.PORT, () => {
+
+app.listen(PORT, () => {
     console.log(`Server is listening on port ${process.env.PORT}`)
 })
